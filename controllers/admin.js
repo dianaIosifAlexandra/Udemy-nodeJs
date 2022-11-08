81764374a5ec33b370497bab77b41de57ba64e26
Iosif Diana-alexandra <diana.iosif@amdaris.com>
2022-11-08 11:01:18 +0200

    create routes + added MVC pattern

---
+++ controllers/admin.js
@@ -0,0 +1,30 @@
+const Product = require('../models/product');
+
+// get add products page 
+exports.getAddProduct = (req, res, next) => {
+    res.render('admin/add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
+}
+
+exports.postAddProduct = (req, res, next) => {
+    const {title, imageUrl, description, price} = req.body;
+
+   const product = new Product(title, imageUrl, description, price);
+   product.save();
+   res.redirect('/');
+}
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin products',
+            path: '/admin/products',
+        });
+    });
+}
---
+++ models/product.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = require('../util/path');
+const myPath = path.join(filePath, 'data', 'products.json');
+
+const getProductsFromFile = (callback) => {
+    fs.readFile(myPath, (err, fileContent) => {
+        if (err) {
+            callback([])
+        }
+        else {
+            callback(JSON.parse(fileContent));
+        }
+    });
+};
+
+module.exports = class Product {
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+        /* in situatia de mai jos: products.push(this); scris direct in save(), this se refera la obiectul creat pe baza clasei 
+         si este exact obiectul pe care vreau sa il adaug */
+        // products.push(this);
+ 
+        getProductsFromFile((products) => {
+            /* this se refera la clasa deoarece este chemat intr-un arrow function, 
+                      daca era un simplu function, this facea referire la scope-ul functiei*/
+            products.push(this);
+
+            fs.writeFile(myPath, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    /*permite apelarea acestei functii direct din clasa, fara a ma folosi de un obiect instantiat */
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
+    }
+}
